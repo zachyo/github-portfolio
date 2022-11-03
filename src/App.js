0917426e317f4ea187b07f84a7dcfb34c8e5f5fc
zacheusIsrael <zacheazy99@yahoo.com>
2022-11-03 15:26:27 +0800

    Nested routes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from "react-router-dom";
 import Homepage from './components/homepage/homepage';
 import Navbar from './components/navbar/navbar';
+import Repos from './components/repos/repos';
 
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Homepage/>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-      {/* <Routes>
-        <Route path='home' element={<Homepage/>} />
-      </Routes> */}
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Homepage />} />
+        <Route exact path="repos" element={<Repos/>} />
+      </Routes>
     </div>
   );
 }
