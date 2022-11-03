15812907144d7e4c1dc24463b122ca642d025007
zacheusIsrael <zacheazy99@yahoo.com>
2022-11-03 15:27:41 +0800

    repos page

--- src/components/repos/repos.jsx
+++ src/components/repos/repos.jsx
@@ -1,5 +1,22 @@
+import useFetch from "../useFetch2";
+
 const Repos = () => {
-    return ( <div className="repos"></div> );
-}
- 
+  const url = `https://api.github.com/users/zachyo/repos`;
+
+  const { loading, error, data } = useFetch(url);
+//   console.log(data);
+
+  if (!loading && error) {
+    return <>Error</>;
+  }
+  return (
+    <div className="repos">
+      Repos pagination
+      {data.map((repo) => {
+        return <div key={repo.id}>{repo.name}</div>;
+      })}
+    </div>
+  );
+};
+
 export default Repos;
