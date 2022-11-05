e8627e39907d68e3f13775bd65291f3cbdd747b5
zacheusIsrael <zacheazy99@yahoo.com>
2022-11-05 05:45:52 +0100

    navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+import './navbar.scss'
+
 const Navbar = () => {
-    return ( <div className="navbar">
-        NAVBAR
-    </div> );
+    return (
+      <div className="navbar">
+        <Link to="/">Home</Link>
+        <Link to="/repos">Repositories</Link>
+      </div>
+    );
 }
  
 export default Navbar;
