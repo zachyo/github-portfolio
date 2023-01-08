60ecba970d2937e8a20908041952301acbd94fa1
zacheusIsrael <zacheazy99@yahoo.com>
2023-01-09 00:15:17 +0100

    Update repo-info.jsx

--- src/components/repo-info/repo-info.jsx
+++ src/components/repo-info/repo-info.jsx
@@ -1,4 +1,4 @@
-import useFetch from "../useFetch";
+import useFetch from "../../utilities/useFetch";
 
 import { useParams } from "react-router-dom";
 import Spinner from "../spinner/spinner.component";
@@ -8,28 +8,27 @@ const RepoInfo = () => {
   const { id } = useParams();
   const url = `https://api.github.com/repos/zachyo/${id}/contents`;
   const { loading, error, data } = useFetch(url);
-  console.log(data)
-  if (loading) {
-    return <Spinner />;
-  }
 
-  if (!loading && error) {
-    return <>Error</>;
-  }
+  const repoList = data?.map((each) => {
+    return each ? (
+      <div className="" key={each.sha}>
+        <p>Name: {each.name}</p>
+        <a href={each.html_url}>View file on github</a>
+      </div>
+    ) : (
+      <h1>There are no files in this repository</h1>
+    );
+  });
 
   return (
     <div className="repo-info">
       <h1>Contents of {id} repository</h1>
-      {data?.map((each) => {
-        return each ? (
-          <div className="" key={each.sha}>
-            <p>Name: {each.name}</p>
-            <a href={each.html_url}>View file on github</a>
-          </div>
-        ) : (
-          <h1>There are no files in this repository</h1>
-        );
-      })}
+      {error ? (
+        <>Failed to fetch. Kindly check your internet connection</>
+      ) : (
+        <></>
+      )}
+      {loading ? <Spinner /> : <>{repoList}</>}
     </div>
   );
 };
